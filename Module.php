9af37d1637d7a4611a5bf1c068078352216dbd90
Denis Petek <dpetek1@gmail.com>
2013-12-04 15:53:04 -0800

    Module.php updated to support composer install

--- Module.php
+++ Module.php
@@ -1,56 +1,7 @@
 <?php
-
-namespace Leptir;
-
-
-use Zend\Console\Adapter\AdapterInterface;
-use Zend\Mvc\ModuleRouteListener;
-use Zend\Mvc\MvcEvent;
-use Zend\Config\Factory;
-
-class Module
-{
-    public function onBootstrap(MvcEvent $e)
-    {
-        $eventManager        = $e->getApplication()->getEventManager();
-        $moduleRouteListener = new ModuleRouteListener();
-        $moduleRouteListener->attach($eventManager);
-    }
-
-    public function getConfig()
-    {
-        $configs = array(
-            __DIR__ . '/config/module.config.php',
-            __DIR__ . '/config/leptir.default.config.php'
-        );
-
-        return Factory::fromFiles($configs);
-    }
-
-    public function getAutoloaderConfig()
-    {
-        return array(
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                ),
-            ),
-        );
-    }
-
-    public function getConsoleUsage(AdapterInterface $console)
-    {
-        return array(
-            'Leptir - background task processor',
-            'Usages',
-            'index.php leptir daemon start|stop|restart [--configuration config_file.ext]',
-            'index.php leptir tester <action> <taskName>'
-        );
-    }
-
-    public function getServiceConfig()
-    {
-        return array();
-    }
-
-}
+/**
+ * This file is placed here for compatibility with Zendframework 2's ModuleManager.
+ * It allows usage of this module even without composer.
+ * The original Module.php is in 'src/DoctrineORMModule' in order to respect PSR-0
+ */
+require_once __DIR__ . '/src/Leptir/Module.php';
---
+++ src/Leptir/Module.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Leptir;
+
+
+use Zend\Console\Adapter\AdapterInterface;
+use Zend\Mvc\ModuleRouteListener;
+use Zend\Mvc\MvcEvent;
+use Zend\Config\Factory;
+
+class Module
+{
+    public function onBootstrap(MvcEvent $e)
+    {
+        $eventManager        = $e->getApplication()->getEventManager();
+        $moduleRouteListener = new ModuleRouteListener();
+        $moduleRouteListener->attach($eventManager);
+    }
+
+    public function getConfig()
+    {
+        $configs = array(
+            __DIR__ . '/config/module.config.php',
+            __DIR__ . '/config/leptir.default.config.php'
+        );
+
+        return Factory::fromFiles($configs);
+    }
+
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+    public function getConsoleUsage(AdapterInterface $console)
+    {
+        return array(
+            'Leptir - background task processor',
+            'Usages',
+            'index.php leptir daemon start|stop|restart [--configuration config_file.ext]',
+            'index.php leptir tester <action> <taskName>'
+        );
+    }
+
+    public function getServiceConfig()
+    {
+        return array();
+    }
+
+}
